1e35f7fc6a44c3ffd97901a0ada7699cda120898
Alex Servin II <servin.alejandroii@gmail.com>
2026-02-28 23:50:01 -0600

    testing word display methods

--- src/components/TypingTest.tsx
+++ src/components/TypingTest.tsx
@@ -1,18 +1,13 @@
-import {
-  useEffect,
-  useMemo,
-  useState,
-  type ChangeEvent,
-  type KeyboardEvent,
-} from "react";
+import { useEffect, useMemo, useState } from "react";
 import { TimedMode } from "../core/engine/modes/TimedMode";
 import { TypingEngine } from "../core/engine/TypingEngine";
 
 const text = "hello world";
 const textArray = text.split("");
+
 export function TypingTest() {
   const engine = useMemo(() => {
-    return new TypingEngine(text, new TimedMode(5000));
+    return new TypingEngine(text, new TimedMode(60000));
   }, []);
 
   const timeLimit = engine.getTimeLimit();
@@ -24,27 +19,49 @@ export function TypingTest() {
     setState({ ...engine.getState() });
   }
 
-  function handleInput(e: ChangeEvent<HTMLTextAreaElement>) {
-    engine.handleCharacter(e.target.value);
-    setState({ ...engine.getState() });
+  function handleInput(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (
+      e.ctrlKey ||
+      e.altKey ||
+      e.metaKey ||
+      e.shiftKey ||
+      e.key === "Escape"
+    ) {
+      return;
+    } else if (e.key === "Backspace") {
+      engine.handleBackspace();
+      setState({ ...engine.getState() });
+    } else {
+      engine.handleCharacter(e.key);
+      setState({ ...engine.getState() });
+    }
+
+    console.log(state.typedCharacters);
   }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      engine.checkTime();
-      setState({ ...engine.getState() });
-    }, 100);
+  function displayWords() {}
 
-    return () => clearInterval(interval);
-  }, [engine]);
+  useEffect(() => {
+    if (state.status === "running") {
+      const interval = setInterval(() => {
+        engine.checkTime();
+        setState({ ...engine.getState() });
+      }, 100);
+
+      return () => clearInterval(interval);
+    }
+  }, [engine, state.status]);
 
   return (
-    <div className="min-h-screen bg-neutral-800">
-      <div className="relative text-red-400 font-bold text-5xl">
-        <textarea onChange={handleInput} className="relative z-50"></textarea>
+    <div className="min-h-screen bg-neutral-800 font-mono">
+      <div className="relative font-bold text-5xl">
+        <textarea
+          onKeyDown={handleInput}
+          className="relative text-transparent z-50"
+        ></textarea>
         <div className="absolute top-0 left-0 text-neutral-500">
-          {textArray.map((char: string) => {
-            return <span>{char}</span>;
+          {textArray.map((c, i) => {
+            return <span>{c}</span>;
           })}
         </div>
       </div>
@@ -63,13 +80,6 @@ export function TypingTest() {
         {timeLimit &&
           (timeLimit / 1000 - engine.getElapsedTime() / 1000).toFixed(2)}
       </div>
-      <div className="text-red-400 font-bold text-5xl">
-        typed characters:{" "}
-        {state.typedCharacters.map((c) => {
-          return <div>{c.char}</div>;
-        })}
-      </div>
-
       <button
         onClick={handleStart}
         className="bg-blue-500 p-3 text-white cursor-pointer"
