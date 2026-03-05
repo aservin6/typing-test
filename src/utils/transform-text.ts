a1c100d837f10a3aa258715f9886d0a565d10ccc
Alex Servin II <servin.alejandroii@gmail.com>
2026-03-04 23:01:17 -0600

    update transformText function

--- src/utils/transform-text.ts
+++ src/utils/transform-text.ts
@@ -1,22 +1,50 @@
 import type { TransformedText } from "../types/types";
 
 export default function transformText(text: string) {
-  let textArray: TransformedText[] = [];
-  let wordIndex = 0;
+  const textArray: TransformedText[] = [];
   let characters: string[] = [];
-  const splitText = text.split("");
+  let wordIndex = 0;
 
-  splitText.forEach((char, index) => {
+  for (const char of text) {
     if (char === " ") {
-      textArray.push({ type: "space" });
-      wordIndex++;
-      characters = [];
-    } else {
-      characters.push(char);
-      if (splitText.at(index + 1) === " " || splitText.length === index + 1) {
-        textArray.push({ type: "word", characters, wordIndex });
+      // Flush word if it exists
+      if (characters.length > 0) {
+        textArray.push({
+          type: "word",
+          characters,
+          wordIndex,
+        });
+        wordIndex++;
+        characters = [];
       }
+
+      // Add space after each word
+      textArray.push({ type: "space" });
+    } else {
+      // Else push char to characters
+      characters.push(char);
     }
-  });
+  }
+  // Final flush (end of input)
+  if (characters.length > 0) {
+    textArray.push({
+      type: "word",
+      characters,
+      wordIndex,
+    });
+  }
   return textArray;
 }
+
+// splitText.forEach((char, index) => {
+//   if (char === " ") {
+//     textArray.push({ type: "space" });
+//     wordIndex++;
+//     characters = [];
+//   } else {
+//     characters.push(char);
+//     if (splitText.at(index + 1) === " " || splitText.length === index + 1) {
+//       textArray.push({ type: "word", characters, wordIndex });
+//     }
+//   }
+// });
